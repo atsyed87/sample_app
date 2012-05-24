86ea29aef0ca9217970013c4567e4f6ef6a4db10
Asad Syed <atsyed87@gmail.com>
2012-05-24 15:21:18 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,7 +17,7 @@ describe User do
     @user = User.new(name: "Example User", email: "user@example.com", 
                      password: "foobar", password_confirmation: "foobar")
   end
-
+  
   subject { @user }
 
   it { should respond_to(:name) }
@@ -25,9 +25,14 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
     
   it { should be_valid }
+  
+  describe "remember token" do
+     before { @user.save }
+     its(:remember_token) { should_not be_blank }
+   end
 
   describe "when name is not present" do
     before { @user.name = " " }
